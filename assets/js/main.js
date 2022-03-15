d67e5b92f4e27c7c9d2b6a30dbfa5d0ba32c25f6
gr492_aav <liveonelasttime@gmail.com>
2022-03-15 18:37:50 +0700

    first done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,3 @@
-// var app = new Vue({
-//     el: '#app',
-//     data: {
-//         message:'Привет, лох!'
-//     }
-// })
-
-// var app1 = new Vue({
-//     el: '#app-1',
-//     data: {
-//         message:'Вы загрузили эту страницу: ' + new Date().toLocaleString()
-//     }
-// })
-
-// var app2 = new Vue({
-//     el: '#app-2',
-//     data: {
-//         seen: true
-//     }
-// })
-
-// var app3 = new Vue({
-//     el: '#app-3',
-//     data:{
-//         todos: [
-//             {text: 'Изучить JavaScript'},
-//             {text: 'Изучить Vue'},
-//             {text: 'Создать что-нибудь классное'}
-//         ]
-//     }
-// })
-
-
-// var app4 = new Vue({
-//     el:'#app-4',
-//     data: {
-//         message:'Привет, вуе'
-//     },
-//     methods: {
-//         reverseMessage: function () {
-//             this.message = this.message.split('').reverse().join('')
-//         }
-//     }
-// })
-
-// var app5 = new Vue({
-//     el: '#app-5',
-//     data: {
-//         message: 'Привет, вуе'
-//     }
-// })
-
-// Vue.component('todo-item', {
-//     props:['todo'],
-//     template:'<li>{{ todo.text }}</li>'
-// })
-
-// var app6 = new Vue({
-//     el: '#app-6',
-//     data: {
-//         groceryList: [
-//             {id:0, text:'Овощи'},
-//             {id:1, text:'Сыр'},
-//             {id:2, text: 'Что там ещё люди едят?'}
-//         ]
-//     }
-// })
-
-
 var list = new Vue({
     el:'#app-6',
     data: {
@@ -90,7 +21,7 @@ Vue.component('todo-item', {
             line: false
         }
     },
-    template:'<li v-bind:class="{ lt: line, pointer: true }" @click="line=!line">{{ todo.text }}<button v-on:click="del">Удалить</button></li>'
+    template:`<li v-bind:class="{ lt: line, pointer: true }" @click="line=true">{{ todo.text }}<button @click="$emit('remove')">X</button></li>`
 })
 
 var app6 = new Vue({
@@ -105,9 +36,5 @@ var app6 = new Vue({
             this.groceryList.push({id:this.id++, text:this.food})
             this.food=''
         },
-        del: function(id){
-          this.$delete(groceryList, )
-        }
     }
-    
 })
