041dd9b76f175ce1e8ebdb8308c80776d6dc6a47
gr492_aav <liveonelasttime@gmail.com>
2022-03-24 12:26:53 +0700

    done

--- lab4/assets/js/main.js
+++ lab4/assets/js/main.js
@@ -1,183 +1,112 @@
-// Vue.component('todo-list', {
-//     props: ['list', 'list_index'],
-//     data: function () {
-//         return {
-//             newItem: '',
-//             colors: ['grey', 'lightPink', 'lightGreen', 'Yellow', 'lightBlue'],
-//         }
-//     },
-//     methods: {
-//         addItem() {
-//             if (this.newItem !== '') {
-//                 vm.records.find(value => value.id === this.list_index).items.push({
-//                     title: this.newItem,
-//                     done: 'none',
-//                     priority: false
-//                 });
-//                 vm.saveItems();
-//                 this.newItem = '';
-//             }
-//         },
-//         deleteRecord() {
-//             vm.records.splice(vm.records.findIndex(value => value.id === this.list_index), 1);
-//             vm.searchResult.splice(vm.searchResult.findIndex(value => value.id === this.list_index), 1);
-//             vm.saveItems();
-//         }
-//
-//     },
-//     template: `
-//     <div class="bg-white border mt-4">
-//         <header class="border-bottom">
-//             <div v-bind:style="{backgroundColor:list.color,height: 10 + 'px' }"></div>
-//                 <div class="d-flex px-4 py-3 justify-content-between">
-//                     <h2>{{list.title}}</h2>
-//                     <div class="col-1 d-flex justify-content-between">
-//                     <img src="assets/img/trash.png" alt="" style="height: 25px;height: 25px;cursor: pointer" @click="deleteRecord">
-//                     <img src="assets/img/fill.png" alt="" style="height: 25px;height: 25px;cursor: pointer" @click="setColor">
-//                 </div>
-//             </div>
-//         </header>
-//
-//         <div>
-//             <transition-group name="todo">
-//                 <todo-item v-for="(item,index) in list.items" :index="index" :item="item" :list_index="list_index" :key="item"></todo-item>
-//             </transition-group>
-//
-//         </div>
-//
-//         <div class=" border-bottom p-3">
-//             <div class="input-group">
-//                 <input type="text" class="form-control border-0 fs-5 shadow-none" aria-label="Text input with checkbox" placeholder="Add new" v-model.lazy="newItem" @keyup.enter="addItem">
-//             </div>
-//         </div>
-//
-//         <div class=" mt-3 px-3">
-//             <p>Created: {{list.time}}</p>
-//         </div>
-//     </div>
-// `
-// });
-//
-// Vue.component('todo-item', {
-//     props: ['item', 'index', 'list_index'],
-//     methods: {
-//         deleteItem() {
-//             vm.records.find(value => value.id === this.list_index).items.splice(this.index, 1);
-//             vm.saveItems();
-//         },
-//         setDone() {
-//             this.item.done = (this.item.done === 'none') ? 'line-through' : 'none'
-//             vm.saveItems();
-//         },
-//         setPriority() {
-//             this.item.priority = !this.item.priority
-//             vm.saveItems();
-//         },
-//     },
-//     template: `
-// <div class="py-3 px-2 pe-4 border-bottom d-flex justify-content-between align-items-center">
-//     <div class="d-flex align-items-center">
-//         <div class="col-1 p-1 me-3">
-//            <img src="assets/img/pin.png" alt="" class="img-fluid" @click="setPriority"  :style="{cursor:'pointer'}">
-//         </div>
-//         <h4 @click="setDone" :style="[item.priority?{color:'red'}:{color:'black'},{textDecoration:item.done,cursor: 'pointer'}]">{{item.title}}</h4>
-//     </div>
-//     <img src="assets/img/trash.png" alt="" style="height: 20px;height: 20px;cursor: pointer" @click="deleteItem">
-// </div>
-//     `
-// });
-//
-//
-// const vm = new Vue({
-//     el: '#app',
-//     data: {
-//         id: 0,
-//         records: [],
-//         searchResult: [],
-//         filterRecords: [],
-//         title: '',
-//         pages: ['main', 'addRecord'],
-//         currentPage: 'main',
-//         searchQuery: '',
-//     },
-//
-//     created() {
-//         this.id = Number(JSON.parse(localStorage.getItem("id")));
-//         this.records = JSON.parse(localStorage.getItem("records") || '[]');
-//     },
-//
-//     computed: {
-//         currentDate() {
-//             let time = new Date();
-//             let hours = (time.getHours() > 9) ? time.getHours() : '0' + time.getHours();
-//             let minutes = (time.getMinutes() > 9) ? time.getMinutes() : '0' + time.getMinutes();
-//             time = hours + ":" + minutes;
-//             return time;
-//         },
-//     },
-//
-//     methods: {
-//
-//         saveItems() {
-//             const parsed = JSON.stringify(this.records);
-//             localStorage.setItem('records', parsed);
-//             localStorage.setItem('id', this.id);
-//         },
-//         addRecord() {
-//             if (this.title === '') {
-//                 return this.move('addRecord');
-//             }
-//             this.records.push({id: this.id, title: this.title, items: [], time: this.currentDate, color: '',})
-//             this.saveItems();
-//             this.title = '';
-//             this.id++;
-//             this.currentPage = 'main';
-//             return true;
-//
-//         }
-//     }
-// });
-
-Vue.component('todo-record', {
-    props:['record'],
+Vue.component('todo-list', {
+    props: ['list', 'list_index'],
     data: function () {
         return {
-
+            newTask: '',
         }
     },
     methods: {
 
+        addTask() {
+            if (this.newTask !== '') {
+                app.records.find(value => value.id === this.list_index).task.push({
+                    title: this.newTask,
+                    done: 'none',
+                });
+                app.saveItems();
+                this.newTask = '';
+            }
+        },
+        deleteRecord() {
+            app.records.splice(app.records.findIndex(value => value.id === this.list_index), 1);
+            app.saveItems();
+        }
+
     },
+    template: `
 
-    template: ''
-})
-Vue.component('todo-task')
+    <div class="bg-white border mt-4 rounded">
+        <header class="border-bottom">
+            <div class="d-flex px-4 py-3 justify-content-between">
+                <h2>{{list.title}}</h2>
+                <div class="col- d-flex justify-content-between">
+                    <div @click="deleteRecord" style="font-size: 35px; cursor: pointer">X</div>
+                </div>
+            </div>
+        </header>
+        
+        <div>
+            <todo-item v-for="(item, index) in list.task" :index="index" :item="item" :list_index="list_index" :key="item"></todo-item>
+        </div>
+        
+        <div class=" border-bottom p-3">
+            <div class="input-group">
+                <input type="text" class="form-control border-0 fs-5 shadow-none" placeholder="Add new" v-model.lazy="newTask">
+                <button class="btn btn-outline-primary bt-md" @click="addTask">Сохранить</button>
+            </div>
+        </div>
+    </div>
+`
+});
 
-let app = new Vue({
-    el: '#todo',
+Vue.component('todo-item', {
+    props: ['item', 'index', 'list_index'],
+    methods: {
+        deleteTask() {
+            app.records.find(value => value.id === this.list_index).task.splice(this.index, 1);
+            app.saveItems();
+        },
+        setDone() {
+            this.item.done = (this.item.done === 'none') ? 'line-through' : 'none'
+            app.saveItems();
+        },
+    },
+    template: `
+    <div class="py-3 px-2 pe-4 border-bottom d-flex justify-content-between align-items-center">
+        <div class="d-flex align-items-center">
+            <h5 @click="setDone" :style="{textDecoration:item.done,cursor: 'pointer'}">{{item.title}}</h5>
+        </div>
+        <div @click="deleteTask" style="font-size: 20px;cursor: pointer">X</div>
+    </div>
+    `
+});
+
+const app = new Vue({
+    el: '#app',
     data: {
         id: 0,
         records: [],
         title: '',
-        taskTitle: '',
-
+        taskName: '',
 
     },
+
+    created() {
+        this.id = Number(JSON.parse(localStorage.getItem("id")));
+        this.records = JSON.parse(localStorage.getItem("records") || '[]');
+    },
+
     methods: {
-        addRecord: function () {
-            this.records.push({id: this.id, name: this.title, tasks: []});
-            this.saveRecord();
-            this.id++;
-            this.title = '';
-        },
-        saveRecord: function () {
+        saveItems() {
             const parsed = JSON.stringify(this.records);
             localStorage.setItem('records', parsed);
+            localStorage.setItem('id', this.id);
         },
-        addTask: function (id) {
-            this.records[id].tasks.push({taskTitle: this.taskTitle});
-            this.saveRecord();
-            this.taskTitle = '';
-        }
+        addRecord() {
+            if (this.title === '') {
+                return false;
+            }
+            this.records.push({
+                id: this.id,
+                title: this.title,
+                task: []
+            });
+            this.saveItems();
+            this.title = '';
+            this.id++;
+            return true;
+
+        },
+
     }
-})
+});
